d506f9b2850fe257ffe8dae722e8b5b791f21142
Kerim Satirli <k.satirli@gmail.com>
2026-02-24 11:13:35 +0100

    adds provider config

---
+++ providers.tf
@@ -0,0 +1,26 @@
+# see https://registry.terraform.io/providers/hashicorp/azurerm/4.61.0/docs
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.azurerm_subscription_id
+  use_cli         = true
+}
+
+# see https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+  ibmcloud_timeout = var.ibmcloud_timeout
+
+  region     = var.ibmcloud_region
+  visibility = var.ibmcloud_visibility
+}
+
+# see https://registry.terraform.io/providers/hashicorp/local/latest/docs
+provider "local" {
+  # The `local` provider has no configuration
+}
+
+# see https://registry.terraform.io/providers/hashicorp/tls/latest/docs
+provider "tls" {
+  # The `tls` provider has no configuration
+}
