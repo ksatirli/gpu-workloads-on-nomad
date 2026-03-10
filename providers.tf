2cfd5d17a8eea3d995aab523239fd71191922268
Kerim Satirli <k.satirli@gmail.com>
2026-03-10 12:11:31 +0100

    cleanup

--- providers.tf
+++ providers.tf
@@ -6,15 +6,6 @@ provider "azurerm" {
   use_cli         = true
 }
 
-# see https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs
-provider "ibm" {
-  ibmcloud_api_key = var.ibmcloud_api_key
-  ibmcloud_timeout = var.ibmcloud_timeout
-
-  region     = var.ibmcloud_region
-  visibility = var.ibmcloud_visibility
-}
-
 # see https://registry.terraform.io/providers/hashicorp/local/latest/docs
 provider "local" {
   # The `local` provider has no configuration
